82825009730431079aecd1298081b8f0b8b46c5b
Eric <fugifugo@gmail.com>
2019-01-22 16:31:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyACKhfZkJFvWqZYNF-Q7OrbSqbU0IHyT_A';
+const API_KEY = 'AIzCKhfZkJFvaSyAbU0qZYNF-QIHyT_AW7OrbSq';
 
 // Create a new component. This component should produce some HTML.
 class App extends Component {
